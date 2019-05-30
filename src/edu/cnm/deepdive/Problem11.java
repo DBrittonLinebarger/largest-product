69eaab48c8b46aa93b693f9125c42e1587a28cd2
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-30 08:04:49 -0600

    bug fixed

--- src/edu/cnm/deepdive/Problem11.java
+++ src/edu/cnm/deepdive/Problem11.java
@@ -2,7 +2,6 @@ package edu.cnm.deepdive;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,14 +10,14 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Problem11 {
 
+public class Problem11 {
   private static int[][] readFile(String filename) throws IOException {
     try (
         InputStream input = new FileInputStream(filename);
         Reader reader = new InputStreamReader(input);
-        BufferedReader buffer = new BufferedReader(reader)
-    ){
+        BufferedReader buffer = new BufferedReader(reader))
+    {
       List<int[]> workList = new LinkedList<>();
       String line;
       while ((line = buffer.readLine()) != null) {
@@ -32,10 +31,8 @@ public class Problem11 {
       return workList.toArray(new int[workList.size()][]);
     }
   }
-
   public static void main(String[] args) throws IOException {
     int[][] data = readFile("largest-product-data.txt");
     System.out.println(Arrays.toString(data));
   }
-
 }
